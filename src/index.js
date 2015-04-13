8f8917c6f8be5aaefa29321fa1632dbd9f93eaa4
Wolfram Kriesing <wk@uxebu.com>
2015-04-13 14:29:25 +0200

    Render the site, very basic.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,33 @@ const url = `https://api.github.com/search/code?q=repo%3Atddbin%2Fkatas+language
 
 atomic.get(url)
   .success((data) => {
-    console.log(data);
+    render(data.items.map((item) => item.path));
   })
   .error((e, xhr) => {
     console.log(':(', xhr);
   })
 ;
+
+import React from 'react';
+
+export default class MainComponent extends React.Component {
+
+  render() {
+    let {paths} = this.props;
+    return (
+      <ol>
+        {paths.map((path) => <li>{tddbinKataLink(path)}</li>)}
+      </ol>
+    );
+  }
+}
+
+const tddbinKataLink = (path) => {
+  const readable = path.replace('katas/es6/language/', '');
+  const link = `http://tddbin.com/#?kata=${path.replace('katas/', '').replace(/\.js$/, '')}`;
+  return <a href={link} target="_blank">{readable}</a>;
+};
+
+const render = (paths) => {
+  React.render(<MainComponent paths={paths}/>, document.querySelector('#katas'));
+};
