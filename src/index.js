7900dc7794b87e74cdbd20ec8f40af7c54007f67
Wolfram Kriesing <wk@uxebu.com>
2015-04-18 01:27:41 +0200

    Make it look a little bit good.

--- src/index.js
+++ src/index.js
@@ -17,21 +17,47 @@ import React from 'react';
 export default class MainComponent extends React.Component {
 
   render() {
-    let {paths} = this.props;
+    let {groups} = this.props;
+    let ret = [];
+    for (let groupName in groups) {
+      ret.push(kataGroup(groupName, groups[groupName]));
+    }
     return (
-      <ol>
-        {paths.map((path) => <li>{tddbinKataLink(path)}</li>)}
-      </ol>
+      <body>
+        <h1>ES6 Katas</h1>
+        <p>Just learn a bit of ES6 daily, take one kata a day and fix it away.</p>
+        {ret}
+        <footer>an <a href="http://uxebu.com">uxebu</a> project, using <a href="http://tddbin.com">tddbin</a></footer>
+      </body>
     );
   }
 }
 
+const kataGroup = (groupName, group) => {
+  return (
+    <div className="group">
+      <h2>{groupName}</h2>
+      {group.map(tddbinKataLink)}
+    </div>
+  );
+};
+
 const tddbinKataLink = (path) => {
-  const readable = path.replace('katas/es6/language/', '');
+  const [group, sub] = path.replace('katas/es6/language/', '').split('/');
   const link = `http://tddbin.com/#?kata=${path.replace('katas/', '').replace(/\.js$/, '')}`;
-  return <a href={link} target="_blank">{readable}</a>;
+  return <a href={link} target="_blank">{sub}</a>;
+};
+
+const groupedPaths = (paths) => {
+  let groups = {};
+  paths.forEach((path) => {
+    const groupName = path.split('/').reverse()[1];
+    if (!groups[groupName]) groups[groupName] = [];
+    groups[groupName].push(path);
+  });
+  return groups;
 };
 
 const render = (paths) => {
-  React.render(<MainComponent paths={paths}/>, document.querySelector('#katas'));
+  React.render(<MainComponent groups={groupedPaths(paths)}/>, document.querySelector('body'));
 };
