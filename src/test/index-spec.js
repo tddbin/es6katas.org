e43f5dcdae8eba544e42f46687ab22fe0ea92961
Wolfram Kriesing <wk@uxebu.com>
2015-04-22 16:53:06 +0200

    First bit for parsing out the data.

--- src/test/index-spec.js
+++ src/test/index-spec.js
@@ -5,3 +5,28 @@ describe('tests run', function() {
     assert.equal(1, 1);
   });
 });
+
+/**
+ - get out all paths out of the JSON
+ - group the katas
+ - sort the kata groups (somehow???)
+ - generate the tddbin link object
+*/
+
+describe('convert github data', function() {
+  it('to a list of kata paths', function() {
+    const githubJson = {
+      items: [
+        {path: "path1"},
+        {path: "path2"},
+        {path: "path3"}
+      ]
+    };
+    
+    assert.deepEqual(getPathList(githubJson), ['path1', 'path2', 'path3']);
+  });
+});
+
+const getPathList = (githubJson) => {
+  return ['path1', 'path2', 'path3'];
+};
