046b10f6e53327483ae36ef6dc4791e9506a30a9
Wolfram Kriesing <wk@uxebu.com>
2015-04-25 12:19:44 +0200

    Deactivate prerendering for now, until we have it refactored to activate it again.

--- scripts/render-html.sh
+++ scripts/render-html.sh
@@ -5,9 +5,9 @@ SRC_ROOT="$ORIGIN_ROOT/src"
 DIST_ROOT="$ORIGIN_ROOT/dist"
 
 cd $ORIGIN_ROOT/src;
-PRERENDERED_HTML=`../node_modules/.bin/babel-node index-server-side.js`;
+#PRERENDERED_HTML=`../node_modules/.bin/babel-node index-server-side.js`;
 cd ..;
 
 cat $SRC_ROOT/index-start.html > $DIST_ROOT/index.html
-echo $PRERENDERED_HTML >> $DIST_ROOT/index.html
+#echo $PRERENDERED_HTML >> $DIST_ROOT/index.html
 cat $SRC_ROOT/index-end.html >> $DIST_ROOT/index.html
