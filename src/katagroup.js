cffd68bb5e6264f0e62cc269fcb34f94c1e22194
Wolfram Kriesing <wk@uxebu.com>
2015-04-25 12:32:15 +0200

    Create the simple KataGroup.

--- src/katagroup.js
+++ src/katagroup.js
@@ -1,3 +1,10 @@
 export default class KataGroup {
+ 
+  static withLinks(name, kataLinks) {
+    var group = new KataGroup();
+    group.name = name;
+    group.kataLinks = kataLinks;
+    return group;
+  }
   
 }
--- src/test/katagroup-spec.js
+++ src/test/katagroup-spec.js
@@ -1,8 +1,17 @@
 import assert from 'assert';
 import KataGroup from '../katagroup.js';
 
-describe('', function() {
-  it('', function() {
+describe('kata group', function() {
+  it('provides the `name` given to it', function() {
+    const name = 'name';
+    const group = KataGroup.withLinks(name);
     
+    assert.equal(group.name, name);
+  });
+  it('provides the `kataLinks`', function() {
+    const kataLinks = [];
+    const group = KataGroup.withLinks('', kataLinks);
+    
+    assert.equal(group.kataLinks, kataLinks);
   });
 });
