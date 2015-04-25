fa4e50e4ad4bd06453748ad6338bb75e75dc40ed
Wolfram Kriesing <wk@uxebu.com>
2015-04-25 17:19:25 +0200

    Convert the github JSON to KataGroups.

--- src/github-search-result.js
+++ src/github-search-result.js
@@ -1,10 +1,50 @@
+import KataGroup from './katagroup.js';
+import KataLink from './katalink.js';
+
 export default class GithubSearchResult {
   
-  static fromJson() {
-    return new GithubSearchResult();
+  static fromJson(githubJson) {
+    const result = new GithubSearchResult();
+    result.githubJson = githubJson;
+    return result;
+  }
+  
+  createKataGroups() {
+    const paths = getPathListFromGithubJson(this.githubJson);
+    const parsePath = (path) => {
+      const groupName = path.split('/').reverse()[1];
+      return {
+        groupName: groupName,
+        path: path
+      };
+    };
+    return paths.map(parsePath).reduce(function(obj, path){
+      const groupName = path.groupName;
+      if (!(groupName in obj)) obj[groupName] = KataGroup.withLinks(groupName);
+      obj[groupName].push(KataLink.fromPath(path.path));
+      return obj;
+    }, {});
   }
   
   toKataGroups() {
-    
+    var obj = this.createKataGroups();
+    return KataGroups.fromObject(obj);
   }
 }
+
+class KataGroups extends Array {
+
+  static fromObject(obj) {
+    const groups = new KataGroups();
+    for (let key in obj) {
+      groups.push(obj[key]);
+    }
+    return groups;
+  }
+  
+}
+
+export const getPathListFromGithubJson = (githubJson) => {
+  return githubJson.items.map((item) => item.path);
+};
+
--- src/test/github-search-result-spec.js
+++ src/test/github-search-result-spec.js
@@ -1,8 +1,13 @@
 import assert from 'assert';
 import GithubSearchResult from '../github-search-result.js';
 
+const fromGithubJsonToKataGroups = (githubJson) => {
+  const searchResult = GithubSearchResult.fromJson(githubJson);
+  return searchResult.toKataGroups();
+};
+
 describe('create kata groups from the github search result', function() {
-  xit('', function() {
+  it('one group is created for the same group name', function() {
     const githubJson = {
       items: [
         {path: "kata/group/file.js"},
@@ -10,10 +15,33 @@ describe('create kata groups from the github search result', function() {
         {path: "kata/group/file2.js"}
       ]
     };
-    
-    const searchResult = GithubSearchResult.fromJson(githubJson);
-    const kataGroups = searchResult.toKataGroups();
-    
-    assert.equal(kataGroups, '???');
+  
+    var kataGroups = fromGithubJsonToKataGroups(githubJson).length;
+    assert.equal(kataGroups, 1);
+  });
+  
+  it('one group is created for the same group name', function() {
+    const githubJson = {
+      items: [
+        {path: "kata/group/file.js"},
+        {path: "kata/group/file1.js"},
+        {path: "kata/group/file2.js"}
+      ]
+    };
+
+    var kataGroups = fromGithubJsonToKataGroups(githubJson);
+    assert.equal(kataGroups[0].name, 'group');
+  });
+  
+  it('two groups are created for the two group names', function() {
+    const githubJson = {
+      items: [
+        {path: "kata/group/file.js"},
+        {path: "kata/group1/file2.js"}
+      ]
+    };
+
+    var kataGroups = fromGithubJsonToKataGroups(githubJson).length;
+    assert.equal(kataGroups, 2);
   });
 });
