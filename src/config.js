d4a3aaed825e77f0b2ff74677360614269175b24
Wolfram Kriesing <wk@uxebu.com>
2015-04-28 22:48:24 +0200

    Use 100 items per page, so we get all the katas.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const GITHUB_URL = 'https://api.github.com/search/code?q=repo%3Atddbin%2Fkatas+language%3AJavaScript+path%3Akatas%2Fes6%2Flanguage'; 
+const itemsPerPage = 100;
+export const GITHUB_URL = `https://api.github.com/search/code?per_page=${itemsPerPage}&q=repo%3Atddbin%2Fkatas+language%3AJavaScript+path%3Akatas%2Fes6%2Flanguage`; 
