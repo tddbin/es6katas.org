2cec7128c140a043a3e6375ed8c61ce2b6d454fa
Wolfram Kriesing <wk@uxebu.com>
2015-04-28 13:02:14 +0200

    A bit more info in the footer.

--- src/components/page.js
+++ src/components/page.js
@@ -4,12 +4,19 @@ export default class Page extends React.Component {
 
   render() {
     let {kataGroups} = this.props;
+    const numKatas = kataGroups.reduce((old, {kataLinks: {length}}) => old + length, 0);
     return (
       <body>
         <h1>ES6 Katas</h1>
         <p>Just learn a bit of ES6 daily, take one kata a day and fix it away.</p>
         <KataGroups groups={kataGroups} />
-        <footer>an <a href="http://uxebu.com">uxebu</a> project, using <a href="http://tddbin.com">tddbin</a></footer>
+        <footer>
+          <a href="http://uxebu.com">uxebu</a> project --- 
+          using <a href="http://tddbin.com">tddbin</a> ---  
+          on <a href="http://github.com/tddbin/es6katas.org">github</a> ---  
+          {numKatas} katas --- 
+          follow <a href="https://twitter.com/es6katas">@es6katas</a>
+        </footer>
         <script src="./index.js" type="application/javascript"></script>
       </body>
     );
