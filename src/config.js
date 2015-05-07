e0c2593d7bab1f5b508bc9e87af7cc3f04ab4d45
Wolfram Kriesing <wk@uxebu.com>
2015-05-07 23:17:26 +0200

    Fix the URL I broke.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
 const itemsPerPage = 100;
-const searchQuery = encodeURIComponent('repo:tddbin/katas+language:JavaScript+path:katas/es6/language');
-export const GITHUB_URL = `https://api.github.com/search/code?per_page=${itemsPerPage}&q=${searchQuery}`; 
+const searchQuery = [
+  encodeURIComponent('repo:tddbin/katas'),
+  encodeURIComponent('language:JavaScript'),
+  encodeURIComponent('path:katas/es6/language')
+];
+export const GITHUB_URL = `https://api.github.com/search/code?per_page=${itemsPerPage}&q=${searchQuery.join('+')}`; 
