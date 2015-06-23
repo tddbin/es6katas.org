63b74a2ca02a26fe2a2ce8066b4662ec87359038
Wolfram Kriesing <wk@uxebu.com>
2015-06-23 09:57:10 +0200

    Make banner act nicely.

--- src/components/page.js
+++ src/components/page.js
@@ -64,15 +64,31 @@ class Kata extends React.Component {
 }
 
 class WorkshopBanner extends React.Component {
+  constructor() {
+    super();
+    this.state = {bannerIsMinimized: false};
+  }
   render() {
     const imageUrl = './workshopbanner.png';
     const workshopUrl = 'http://www.uxebu.com/all-workshops/es6-and-react-js/';
     var classes = classNames({
       'workshop-banner': true,
-      'fade-in': this.props.showWorkshopBanner
+      'fade-in': this.props.showWorkshopBanner,
+      'small': this.state.bannerIsMinimized
     });
     return <div className={classes}>
-      <a href={workshopUrl}><img src={imageUrl} width="50%" /></a>
+      <div className="close-button-container"><button className="close" onClick={this.minimizeBanner.bind(this)}>[x]</button></div>
+      <a href={workshopUrl} onMouseOver={this.maximizeBanner.bind(this)}>
+        <img src={imageUrl} alt="ES6+reactjs workshop" width="125" height="195" />
+      </a>
     </div>
+  }
+  
+  minimizeBanner() {
+    this.setState({bannerIsMinimized: true});
+  }
+  
+  maximizeBanner() {
+    this.setState({bannerIsMinimized: false});
   }
 }
