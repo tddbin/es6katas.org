8947eac508c8d6609e19c5d8bf0272ee9def5586
Wolfram Kriesing <wk@uxebu.com>
2015-06-26 22:16:43 +0200

    Generate the RSS feed.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const GROUPED_METADATA_URL = 'http://katas.tddbin.com/katas/es6/language/__grouped__.json';
+export const FLAT_METADATA_URL = 'http://katas.tddbin.com/katas/es6/language/__all__.json';
