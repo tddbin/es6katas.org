c8fc67252a674b903a4798da04f0f7b0305f3203
Wolfram Kriesing <wk@uxebu.com>
2015-10-15 22:41:18 +0100

    Extracting and improve names.

--- src/kata.js
+++ src/kata.js
@@ -1,13 +1,23 @@
-const pathToLink = (path) => {
+const rawPathToEs6KataLink = (path) => {
   return `http://tddbin.com/#?kata=es6/language/${path}`;
 };
 
 export default class Kata {
+  
   static fromRawItem(rawItem) {
     let kata = new Kata();
-    Object.keys(rawItem).forEach(key => kata[key] = rawItem[key]);
-    kata.url = pathToLink(rawItem.path || '');
+    kata.initializePropertiesFromRawItem(rawItem);
+    kata.url = rawPathToEs6KataLink(rawItem.path || '');
     kata.id = parseInt(kata.id);
     return kata;
   }
+  
+  initializePropertiesFromRawItem(rawItem) {
+    this.allKeysFromRawItem(rawItem).forEach(key => this[key] = rawItem[key]);
+  }
+
+  allKeysFromRawItem(rawItem) {
+    return Object.keys(rawItem);
+  }
+  
 }
