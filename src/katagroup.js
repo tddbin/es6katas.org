0c3cf9273f3bef100484b50bbdb1968cddc06717
Wolfram Kriesing <wk@uxebu.com>
2015-10-15 22:49:07 +0100

    Remove unnecessary conversion, that is done now at kata creation time.

--- src/katagroup.js
+++ src/katagroup.js
@@ -15,7 +15,7 @@ export default class KataGroup {
   }
   
   sortByName() {
-    this.katas.sort((kata1, kata2) => toInt(kata1.id) < toInt(kata2.id) ? -1 : 1);
+    this.katas.sort((kata1, kata2) => kata1.id < kata2.id ? -1 : 1);
   }
 
   get highestId() {
@@ -25,5 +25,3 @@ export default class KataGroup {
     return this.katas[this.katas.length - 1].id;
   }
 }
-
-const toInt = (string) => Number.parseInt(string, 10);
