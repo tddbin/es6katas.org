9b6950f840d5a31cc3eb84595e335452891d9365
Wolfram Kriesing <wk@uxebu.com>
2015-10-20 21:01:56 +0200

    Better function name.

--- src/metadata.js
+++ src/metadata.js
@@ -25,7 +25,7 @@ class KataGroups {
     const groups = new KataGroups();
     groups.initializePropertiesFromRawObject(obj);
     groups.sortByNumberOfLinks();
-    groups.moveNewestKataToBeginning();
+    groups.moveGroupWithNewestKataToBeginning();
     return groups;
   }
 
@@ -62,7 +62,7 @@ class KataGroups {
     return this.groups.reduce(groupWithHighestId, {highestId:0})
   }
   
-  moveNewestKataToBeginning() {
+  moveGroupWithNewestKataToBeginning() {
     this.moveToBeginning(this.groupWithNewestKata());
   }
   
